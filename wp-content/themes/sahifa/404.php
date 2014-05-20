4d5697799729a9817b15bfc6e3074d2bd800a791
Roc Teis Segura <roc.laperruquera@gmail.com>
2014-05-20 11:35:39 +0200

    Traducció de literals.

--- wp-content/themes/sahifa/404.php
+++ wp-content/themes/sahifa/404.php
@@ -3,7 +3,7 @@
 			
 		<div class="post-listing post error404">
 			<div class="post-inner">
-				<h2 class="post-title"><?php _e( 'Not Found', 'tie' ); ?></h2>
+				<h2 class="post-title"><?php _e( 'ERROR!', 'tie' ); ?></h2>
 				<div class="clear"></div>
 				<div class="entry">
 					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'tie' ); ?></p>
